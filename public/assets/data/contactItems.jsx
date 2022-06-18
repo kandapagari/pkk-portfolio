a269451959d34e376e4bb424f8bbe2d47b4190fb
Pavan Kumar Kandapagari <40955141+kandapagari@users.noreply.github.com>
2022-06-18 22:51:18 +0000

    correction

--- public/assets/data/contactItems.jsx
+++ public/assets/data/contactItems.jsx
@@ -5,7 +5,7 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 const contactItems = [
     { icon: FaLinkedinIn, link: "https://www.linkedin.com/in/kandapagari/" },
     { icon: FaGithub, link: "https://github.com/kandapagari" },
-    { icon: AiOutlineMail, link: "mailto://785pavan@gmail.com" },
+    { icon: AiOutlineMail, link: "mailto:785pavan@gmail.com" },
     { icon: BsFillPersonLinesFill, link: "/#contact" }
 ];
 
