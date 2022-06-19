b7df83f7e4de0794be962156fd0928b9d4508e4f
Pavan Kumar Kandapagari <40955141+kandapagari@users.noreply.github.com>
2022-06-19 22:56:06 +0200

    fix: padding for resume

--- components/ProjectsDummy.jsx
+++ components/ProjectsDummy.jsx
@@ -5,8 +5,12 @@ const ProjectsDummy = () => {
     return (
         <div id='projects_dummy' className='w-full'>
             <div className='max-w-[1240px] mx-auto px-2 py-16'>
+                <p className='py-8'>
+                    {"  "}
+                    {" "}
+                </p>
                 <p className="uppercase text-xl tracking-widest text-[#5651e5]">
-                    <a className='underline text-blue-500'
+                    <a className='underline text-blue-500 py-8'
                         href="/files/resume.pdf"
                         alt="alt text"
                         target="_blank"
@@ -16,7 +20,7 @@ const ProjectsDummy = () => {
                     </a>
                 </p>
             </div>
-                {/* <object className='max-w-[1240px] mx-auto px-2 py-16' data="/files/resume.pdf" type="application/pdf">
+            {/* <object className='max-w-[1240px] mx-auto px-2 py-16' data="/files/resume.pdf" type="application/pdf">
                     <iframe
                         className='max-w-[1240px] mx-auto px-2 py-16'
                         src="https://docs.google.com/viewer?url=your_url_to_pdf&embedded=true"
