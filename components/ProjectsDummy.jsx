063c65d4fb7beadaf9bd1f4891125b1860573531
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2022-06-21 13:52:47 +0200

    fix: logo change

--- components/ProjectsDummy.jsx
+++ components/ProjectsDummy.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import {GiClick} from 'react-icons/gi';
+import { BiLinkExternal } from 'react-icons/bi';
 
 const ProjectsDummy = () => {
 
     return (
         <div id='projects_dummy' className='w-full'>
             <div className='max-w-[1240px] mx-auto px-2 py-16'>
-                <p className='py-8'>
-                    {"  "}
-                    {" "}
-                </p>
-                <p className="uppercase text-xl tracking-widest text-[#5651e5] hover:scale-105 ease-in duration-100 cursor-pointer">
+                <div className="flex flex-col uppercase text-xl tracking-widest text-[#5651e5] hover:scale-y-110 ease-in duration-100 cursor-pointer">
                     <a className='py-8'
                         href="/files/resume.pdf"
                         alt="alt text"
                         target="_blank"
                         rel="noopener noreferrer">
-                        Resume <GiClick />
+                        Resume<BiLinkExternal />
                     </a>
-                </p>
+                </div>
             </div>
             {/* <object className='max-w-[1240px] mx-auto px-2 py-16' data="/files/resume.pdf" type="application/pdf">
                     <iframe
